6f572a679b39d7de8099e0d3c057c0e7b438e916
Sponge <804927486@qq.com>
2018-02-02 14:09:55 +0800

    全部课程页面更改中

--- client/pages/course/course.js
+++ client/pages/course/course.js
@@ -6,63 +6,96 @@ Page({
    */
   data: {
     inputShowed: false,
-    inputVal: ""
+    inputVal: "",
+    normalList: [
+      {
+        url: "../allCourse/allCourse", title: "全部课程"
+      },
+      {
+        url: "../process/process", title: "工艺",
+        children: [
+          {
+            url: "", title: "工艺AAA"
+          },
+          {
+            url: "", title: "工艺BBB"
+          },
+          {
+            url: "", title: "工艺CCC"
+          }
+        ]
+      },
+      {
+        url: "../management/management", title: "管理",
+        children: [
+          {
+            url: "", title: "管理AAA"
+          },
+          {
+            url: "", title: "管理BBB"
+          },
+          {
+            url: "", title: "管理CCC"
+          }
+        ]
+      }
+    ]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   },
   showInput: function () {
     this.setData({
